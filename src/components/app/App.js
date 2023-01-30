d4812e5144298268de5cb9386216f92c239ea59d
Roman <darkkisblack1994@yandex.ru>
2023-01-30 22:17:27 +0700

    added router, dynamic routers

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,49 +1,26 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import ComicsList from "../comicsList/ComicsList";
-import AppBanner from "../appBanner/AppBanner"
-
-import decoration from '../../resources/img/vision.png';
+import { MainPage, ComicsPage, Page404, SingleComicPage } from "../pages";
 
 const App = () => {
 
-    const [selectedChar, setChar] = useState(null);
 
-    const onCharSelected = (id) => {
-        setChar(id)
-    }
 
     return (
-            <div className="app">
-                <AppHeader/>
-                <main>
-                    {/* <ErrorBoundary>
-                        <RandomChar/>
-                    </ErrorBoundary>
-                    
-                    <div className="char__content">
-                        <ErrorBoundary>
-                            <CharList onCharSelected={onCharSelected}/>
-                        </ErrorBoundary>
-                        
-                        <ErrorBoundary>
-                            <CharInfo charId={selectedChar}/>
-                        </ErrorBoundary>
-                        
-                    </div>
-                    <img className="bg-decoration" src={decoration} alt="vision"/> */}
-                    <AppBanner/>
-                    <ErrorBoundary>
-                        <ComicsList></ComicsList>
-                    </ErrorBoundary>
-                    
-                </main>
-            </div>
-    )    
+      <Router>
+        <div className="app">
+          <AppHeader />
+          <main>
+            <Routes>
+              <Route path="/" element={<MainPage/>}/>
+              <Route path="/comics" element={<ComicsPage/>}/>
+              <Route path="*" element={<Page404/>}/>
+              <Route path="comics/:comicId" element={<SingleComicPage/>}/>
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    );    
 
 
 }
--- src/components/comicsList/ComicsList.js
+++ src/components/comicsList/ComicsList.js
@@ -3,6 +3,7 @@ import './comicsList.scss';
 import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
+import { Link } from 'react-router-dom';
 const ComicsList = () => {
 
     const [comicsList, setComicsList] = useState([]);
@@ -37,17 +38,15 @@ const ComicsList = () => {
         setComicsEnded(ended)
     }
 
-
-
     function renderItems (arr){
-        const items = arr.map((item, i) => {
+        const items = arr.map((item) => {
             return (
-                <li className="comics__item" key={i}>
-                    <a href="#">
+                <li className="comics__item" key={item.id}>
+                    <Link to={`/comics/${item.id}`}>
                         <img src={item.thumbnail} alt={item.title} className="comics__item-img"/>
                         <div className="comics__item-name">{item.title}</div>
                         <div className="comics__item-price">{item.price}</div>
-                    </a>
+                    </Link>
                 </li>
             )
 
